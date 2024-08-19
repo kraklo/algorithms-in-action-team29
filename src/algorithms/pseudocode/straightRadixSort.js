2c6ea7f0a63fd40d52fcead58b4a85bc3f9d769a
kraklo <evanliapakis@gmail.com>
2024-08-19 14:47:54 +1000

    Add explanations to the pseudocode

--- src/algorithms/pseudocode/straightRadixSort.js
+++ src/algorithms/pseudocode/straightRadixSort.js
@@ -20,11 +20,14 @@ Radixsort(A, n) // Sort array A[1]..A[n] in ascending order. \\B 1
     \\Expl}
 
     for each bit up to maximum bit    \\Ref RSFor
-    \\Expl{  test
+    \\Expl{  We stop at the maximum bit so that we do not waste time
+      looking at bits that we know will be 0.
     \\Expl}
     \\In{
         Countingsort(A, k, n)    \\Ref Countingsort
-        \\Expl{  test
+        \\Expl{  Straight Radix Sort uses Counting Sort to stably sort the
+          array treating each bit of the number as the number in Counting
+          Sort.
         \\Expl}
     \\In}
 
@@ -51,13 +54,18 @@ for i <- maxBit downto 0
 Countingsort
 // Countingsort(A, k, n) \\B 3
 Count number of 1s and 0s in B    \\Ref CountNums
-\\Expl{  test
+\\Expl{  We count the number of 1s and 0s as these are the only two
+  digits we are concerned with in each iteration of counting sort.
 \\Expl}
 Cumulatively sum counts    \\Ref CumSum
-\\Expl{  test
+\\Expl{  Counting sort requires us to add the previous count to each
+  successive count so that we know the correct place to put each element
+  in the final array.
 \\Expl}
 Populate new array C with sorted numbers    \\Ref Populate
-\\Expl{  test
+\\Expl{  We place each element in the count-1 spot of the new array as
+  this is the correct placement for each element sorted in respect to
+  the current significant bit.
 \\Expl}
 Copy C back to A
 \\Code}
@@ -68,7 +76,11 @@ CountNums
 for num in A
 \\In{
     bit <- kth bit in num    \\Ref KthBit
-    \\Expl{  test
+    \\Expl{  We use a mask to isolate the kth bit we want to look at
+      (as a bitwise and operation with a 1 bit-shifted left k times 
+      isolates that digit), and then bit-shift that right k times so
+      that the value of the number will be either 0 or 1 depending what
+      the significant bit was.
     \\Expl}
     B[bit] <- B[bit]+1
 \\In}
@@ -89,11 +101,16 @@ B[1] = B[0] + B[1]
 Populate
 // Populate new array C with sorted numbers \\B 6
 for each num in A in reverse order    \\Ref PopFor
-\\Expl{  test
+\\Expl{  We go in reverse order so that we preserve the order of each number.
+  This is CRUCIAL in radix sort as the counting sort MUST be stable.
 \\Expl}
 \\In{
     bit <- kth bit in num    \\Ref KthBit
-    \\Expl{  test
+    \\Expl{  We use a mask to isolate the kth bit we want to look at
+      (as a bitwise and operation with a 1 bit-shifted left k times 
+      isolates that digit), and then bit-shift that right k times so
+      that the value of the number will be either 0 or 1 depending what
+      the significant bit was.
     \\Expl}
     B[bit] = B[bit]-1
     C[B[bit]] = num \\B 8
