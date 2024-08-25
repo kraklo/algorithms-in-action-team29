6daa7765a3cac719bf86120778e7d4e16dc343bd
kamzhanyue <kamzhanyue@gmail.com>
2024-08-25 16:58:22 +1000

    Init MSD Radix Sort Controller

--- src/algorithms/controllers/MSDRadixSort.js
+++ src/algorithms/controllers/MSDRadixSort.js
@@ -1,1 +1,68 @@
-export default {};
+import ArrayTracer from '../../components/DataStructures/Array/Array1DTracer';
+import { areExpanded } from './collapseChunkPlugin';
+
+const SRS_BOOKMARKS = {
+    radix_sort: 1,
+    max_number: 2,
+    counting_sort_for_loop: 3,
+    counting_sort: 4,
+    count_nums: 5,
+    cumulative_sum: 6,
+    populate_array: 7,
+    populate_for_loop: 8,
+    insert_into_array: 9,
+    copy: 10,
+    done: 11,
+};
+
+const highlight = (vis, index, isPrimaryColor = true) => {
+    if (isPrimaryColor) {
+        vis.array.select(index);
+    } else {
+        vis.array.patch(index);
+    }
+};
+
+const unhighlight = (vis, index, isPrimaryColor = true) => {
+    if (isPrimaryColor) {
+        vis.array.deselect(index);
+    } else {
+        vis.array.depatch(index);
+    }
+};
+
+const bitAtIndex = (num, index) => {
+    return (num & (1 << index)) >> index;
+};
+
+export default {
+    initVisualisers() {
+        return {
+            array: {
+                instance: new ArrayTracer('array', null, 'Array view', { arrayItemMagnitudes: true }), // Label the input array as array view
+                order: 0,
+            },
+        }
+    },
+
+    /**
+     *
+     * @param {object} chunker
+     * @param {array} nodes array of numbers needs to be sorted
+     */
+    run(chunker, { nodes }) {
+        let A = [...nodes];
+        const n = A.length;
+
+        console.log('Run MSD Radix Sort')
+
+        chunker.add(SRS_BOOKMARKS.radix_sort,
+            (vis, array) => {
+                vis.array.set(array, 'MSDRadixSort');
+            },
+            [nodes]
+        );
+
+        return A;
+    }
+};
