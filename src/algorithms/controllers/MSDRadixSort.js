a1e06a976528910e49f57a11bf06de85c61bcbd8
kamzhanyue <kamzhanyue@gmail.com>
2024-09-14 16:13:54 +1000

    Fix Bugs and Pseudocode for MSDRadixSort

--- src/algorithms/controllers/MSDRadixSort.js
+++ src/algorithms/controllers/MSDRadixSort.js
@@ -1,8 +1,29 @@
 import ArrayTracer from '../../components/DataStructures/Array/Array1DTracer';
+import {
+  areExpanded,
+} from './collapseChunkPlugin';
+
+// see stackFrameColour in index.js to find corresponding function mapping to css
+const STACK_FRAME_COLOR = {
+  No_color: 0,
+  In_progress_stackFrame: 1,
+  Current_stackFrame: 2,
+  Finished_stackFrame: 3,
+  I_color: 4,
+  J_color: 5,
+  P_color: 6, // pivot
+};
+
+const VIS_VARIABLE_STRINGS = {
+  i_left_index: 'i',
+  j_right_index: 'j',
+  i_eq_0: 'i==0',
+  j_eq_0: 'j==0',
+  pivot: 'pivot',
+};
 
 const MSD_BOOKMARKS = {
   start: 1,
-
   get_mask: 100,
   first_pass: 200,
   base_case: 300,
@@ -17,6 +38,34 @@ const MSD_BOOKMARKS = {
   sort_right: 500,
   done: 5000
 };
+
+
+const update_vis_with_stack_frame = (a, stack_frame, stateVal) => {
+  let left, right,  depth;
+  [left, right,  depth] = stack_frame;
+
+  for (let i = left; i <= right; i += 1) {
+    // each element in the vis stack is a tuple:
+    // 0th index is for base color,
+    // 1th index is for pivot, i, j colors
+    a[depth][i] = { base: stateVal, extra: [] };
+  }
+  return a;
+}
+
+const assert = (condition, message) => {
+  if (!condition) {
+    throw new Error(message || 'Assertion failed');
+  }
+}
+
+const isPartitionExpanded = () => {
+  return areExpanded(['Partition'])
+}
+
+const isRecursionExpanded = () => {
+  return areExpanded(['QuicksortFirst']) || areExpanded(['QuicksortSecond']);
+}
 
 const highlight = (vis, index, isPrimaryColor = true) => {
     if (isPrimaryColor) {
@@ -47,6 +96,13 @@ const getMaximumBit = (arr) => {
     return maxBit
 }
 
+const assignVar = (vis, variableName, index) => {
+  if (index === undefined)
+    vis.array.removeVariable(variableName)
+  else
+    vis.array.assignVariable(variableName, index)
+}
+
 export default {
     initVisualisers() {
         return {
@@ -65,22 +121,177 @@ export default {
     run(chunker, { nodes }) {
       let A = [...nodes]
       let n = A.length
+      console.log('start')
 
-      function partition(arr, left, right, mask) {
+
+      // ----------------------------------------------------------------------------------------------------------------------------
+      // Define 'global' variables
+      // ----------------------------------------------------------------------------------------------------------------------------
+
+      const entire_num_array = nodes;
+      let max_depth_index = -1; // indexes into 2D array, starts at zero
+      const finished_stack_frames = []; // [ [left, right,  depth], ...]  (although depth could be implicit this is easier)
+      const real_stack = []; // [ [left, right,  depth], ...]
+
+      // ----------------------------------------------------------------------------------------------------------------------------
+      // Define helper functions
+      // ----------------------------------------------------------------------------------------------------------------------------
+
+      const refresh_stack = (vis, cur_real_stack, cur_finished_stack_frames, cur_i, cur_j, cur_pivot_index, cur_depth) => {
+        // XXX
+        // We can't render the -1 index in the array
+        // For now we display i==0/j==0 at left of array if appropriate
+        let cur_i_too_low;
+        let cur_j_too_low;
+        if (cur_i === -1) {
+          cur_i = undefined;
+          cur_i_too_low = 0;
+        } else {
+          cur_i_too_low = undefined;
+        }
+        if (cur_j === -1) {
+          cur_j = undefined;
+          cur_j_too_low = 0;
+        } else {
+          cur_j_too_low = undefined;
+        }
+
+        assert(vis.array);
+        assert(cur_real_stack && cur_finished_stack_frames);
+
+        if (!isPartitionExpanded()) {
+          // j should not show up in vis if partition is collapsed
+          cur_j = undefined;
+          cur_j_too_low = undefined;
+        }
+
+        if (!isPartitionExpanded() && !isRecursionExpanded()) {
+          // i should not show up in vis if partition + recursion is collapsed
+          cur_i = undefined;
+          cur_i_too_low = undefined;
+        }
+
+        vis.array.setStackDepth(cur_real_stack.length);
+        vis.array.setStack(
+          derive_stack(cur_real_stack, cur_finished_stack_frames, cur_i, cur_j, cur_pivot_index, cur_depth)
+        );
+
+        assign_i_j(vis, VIS_VARIABLE_STRINGS.i_left_index, cur_i);
+        assign_i_j(vis, VIS_VARIABLE_STRINGS.i_eq_0, cur_i_too_low);
+        assign_i_j(vis, VIS_VARIABLE_STRINGS.pivot, cur_pivot_index);
+        assign_i_j(vis, VIS_VARIABLE_STRINGS.j_right_index, cur_j);
+        assign_i_j(vis, VIS_VARIABLE_STRINGS.j_eq_0, cur_j_too_low);
+      };
+
+
+      function derive_stack(cur_real_stack, cur_finished_stack_frames, cur_i, cur_j, cur_pivot_index, cur_depth) {
+        // return 2D array stack_vis containing color values corresponding to stack frame states and indexes in those stack frames
+        // for visualise this data
+
+        let stack_vis = [];
+
+        for (let i = 0; i < max_depth_index + 1; i++) {
+          // for whatever reason fill() does not work here... JavaScript
+          stack_vis.push(
+            [...Array.from({ length: entire_num_array.length })].map(() => ({
+              base: STACK_FRAME_COLOR.No_color,
+              extra: [],
+            })),
+          );
+        }
+
+        cur_finished_stack_frames.forEach((stack_frame) => {
+          stack_vis = update_vis_with_stack_frame(
+            stack_vis,
+            stack_frame,
+            STACK_FRAME_COLOR.Finished_stackFrame,
+          );
+        });
+
+        cur_real_stack.forEach((stack_frame) => {
+          stack_vis = update_vis_with_stack_frame(
+            stack_vis,
+            stack_frame,
+            STACK_FRAME_COLOR.In_progress_stackFrame,
+          );
+        });
+
+        if (cur_real_stack.length !== 0) {
+          stack_vis = update_vis_with_stack_frame(
+            stack_vis,
+            cur_real_stack[cur_real_stack.length - 1],
+            STACK_FRAME_COLOR.Current_stackFrame,
+          );
+        }
+
+        if (cur_depth === undefined) {
+          return stack_vis;
+        }
+
+        if (cur_pivot_index !== undefined) {
+          stack_vis[cur_depth][cur_pivot_index].extra.push(STACK_FRAME_COLOR.P_color);
+        }
+
+        if (!isPartitionExpanded()) { return stack_vis; }
+
+        if (cur_i !== undefined) {
+          stack_vis[cur_depth][cur_i].extra.push(STACK_FRAME_COLOR.I_color);
+        }
+
+        if (cur_j !== undefined) {
+          stack_vis[cur_depth][cur_j].extra.push(STACK_FRAME_COLOR.J_color);
+        }
+
+        return stack_vis;
+      }
+
+      const assign_i_j = (vis, variable_name, index) => {
+        if (index === undefined) { vis.array.removeVariable(variable_name); return; }
+        vis.array.assignVariable(variable_name, index);
+      }
+
+
+
+
+
+
+
+      // ----------------------------------------------------------------------------------------------------------------------------
+      // Real code goes here
+      // ----------------------------------------------------------------------------------------------------------------------------
+      const partition = (arr, left, right, mask) => {
         let i = left
-        chunker.add(MSD_BOOKMARKS.set_i)
         let j = right
-        chunker.add(MSD_BOOKMARKS.set_j)
+        chunker.add(MSD_BOOKMARKS.set_i,
+          (vis) => {
+            assignVar(vis, 'left', left)
+            assignVar(vis, 'i', left)
+          }, [A, left])
+        chunker.add(MSD_BOOKMARKS.set_j,
+          (vis) => {
+            assignVar(vis, 'right', right)
+            assignVar(vis, 'j', right)
+          }, [A, right])
         while (i <= j) {
-          chunker.add(MSD_BOOKMARKS.partition_left)
           // Build the left group until it reaches the mask (find the big element)
           while (i <= right && ((arr[i] >> mask & 1)) === 0) {
+            // Update the position of i on the array
+            console.log(i)
+            // chunker.add(MSD_BOOKMARKS.partition_left,
+            //     (vis) => {
+            //       console.log('Assigning i  to ', i)
+            //       assignVar(vis, 'i', i)
+            //   }, [i])
             i++
           }
-          chunker.add(MSD_BOOKMARKS.partition_right)
           // Build the right group until it fails the mask (find the small element)
           while (j >= left && ((arr[j] >> mask & 1)) === 1) {
+            // Update the position of j on the array
             j--
+            // chunker.add(MSD_BOOKMARKS.partition_right,
+            //     (vis) => {
+            //       assignVar(vis, 'j', j)
+            //   }, [j])
           }
 
           // Swap if the bigger element is not in the right place
@@ -89,8 +300,9 @@ export default {
             chunker.add(MSD_BOOKMARKS.swap,
               (vis, array) => {
                   vis.array.set(array, 'MSDRadixSort')
+                  vis.array.swapElements(i, j)
               },
-              [A]
+              [A, i, j]
             )
           }
         }
@@ -99,32 +311,32 @@ export default {
 
       }
 
-      function msdRadixSortRecursive(arr, left, right, mask) {
-          // Base case: If the array has 1 or fewer elements or mask is less than 0, stop
-          chunker.add(MSD_BOOKMARKS.base_case)
-        if (left < right && mask > 0) {
+      const msdRadixSortRecursive = (arr, left, right, mask) => {
+        // Base case: If the array has 1 or fewer elements or mask is less than 0, stop
+        chunker.add(MSD_BOOKMARKS.base_case)
+        if (left < right && mask >= 0) {
           const mid = partition(arr, left, right, mask)
+          chunker.add(MSD_BOOKMARKS.sort_left,
+            (vis, array) => {
+                vis.array.set(array, 'MSDRadixSort')
+            },
+            [A]
+          )
           msdRadixSortRecursive(arr, left, mid - 1, mask - 1)
-          // chunker.add(MSD_BOOKMARKS.sort_left,
-          //   (vis, array) => {
-          //       vis.array.set(array, 'MSDRadixSort')
-          //   },
-          //   [A]
-          // )
+          chunker.add(MSD_BOOKMARKS.sort_right,
+            (vis, array) => {
+                vis.array.set(array, 'MSDRadixSort')
+            },
+            [A]
+          )
           msdRadixSortRecursive(arr, mid, right, mask - 1)
-          // chunker.add(MSD_BOOKMARKS.sort_right,
-          //   (vis, array) => {
-          //       vis.array.set(array, 'MSDRadixSort')
-          //   },
-          //   [A]
-          // )
         }
       }
 
       // Initialise the array on start
       chunker.add(MSD_BOOKMARKS.start,
         (vis, array) => {
-            vis.array.set(array, 'MSDRadixSort');
+            vis.array.set(array, 'MSDRadixSort')
         },
         [nodes]
       )
@@ -138,7 +350,6 @@ export default {
           [maxIndex]
       )
       chunker.add(MSD_BOOKMARKS.first_pass)
-
 
       const mask = getMaximumBit(A);
       msdRadixSortRecursive(A, 0, n-1, mask);
--- src/algorithms/pseudocode/MSDRadixSort.js
+++ src/algorithms/pseudocode/MSDRadixSort.js
@@ -8,20 +8,18 @@ NOTE that j can start off the RHS of the array.
 
 \\Code{
 Main
-MSDRadixSort(A, n) // Sort array A[1]..A[n] in ascending order. \\B 1
+Rexsort(A, n) // Sort array A[1]..A[n] in ascending order. \\B 1
 \\In{
-    \\Note{ implementation should scan data
-    \\Note}
-
-    mask <- GetMaximumBit(A) \\Ref MaximumBit
+    mask <- maximum bit used \\B 100
     \\Expl{ mask is a power of two (a bit string with a single "1" in it). We
       start with the mask bit being at least as big as any bit that is "1" in
       the data. This can be determined from the word size used to represent
       the data or by scanning through the data (we do the latter here
       because only small examples are used).
     \\Expl}
-
-    MSDRadixSortRecursive(A, 0, n-1, mask) \\B 200
+    \\Note{ implementation should scan data
+    \\Note}
+    RexsortRecursive(A, 1, n, mask) \\B 200
     \\Expl{  We need left and right indices because the code is recursive
         and both may be different for recursive calls. For each call, all
         elements in the array segment must have the same pattern
@@ -29,7 +27,7 @@ MSDRadixSort(A, n) // Sort array A[1]..A[n] in ascending order. \\B 1
     \\Expl}
 \\In}
 //======================================================================
-MSDRadixSortRecursive(A, left, right, mask) // Sort array A[left]..A[right] using bits up to mask
+RexsortRecursive(A, left, right, mask) // Sort array A[left]..A[right] using bits up to mask
 \\Expl{
 Only the mask bit and smaller bits are used for sorting; higher bits
 should be the same for all data in the array segment.
@@ -39,7 +37,7 @@ should be the same for all data in the array segment.
             elements in the array segment or no bits left, do nothing).
     \\Expl}
     \\In{
-        mid <- Partition(A, left, right, mask)    \\Ref Partition
+        Partition array segment using mask    \\Ref Partition
         \\Expl{ This is where most of the work of MSDRadixSort gets done.
                 We start with an unordered array segment, and finish
                 with an array segment containing elements with 0 as the
@@ -58,26 +56,15 @@ should be the same for all data in the array segment.
 \\Code}
 
 \\Code{
-MaximumBit
-maxNumber <- max(A) \\B 100
-mask <- 0 \\B 150
-while maxNumber > 0
-\\In{
-    maxNumber <- maxNumber/2
-    mask <- mask+1
-\\In}
-\\Code}
-
-\\Code{
 MSDRadixSortLeft
 // *Recursively* sort first part: \\B 400
-MSDRadixSortRecursive(A, left, mid-1, mask-1)
+RexsortRecursive(A, left, i-1, mask-1)
 \\Code}
 
 \\Code{
 MSDRadixSortRight
 // *Recursively* sort first part: \\B 500
-MSDRadixSortRecursive(A, mid, right, mask-1)
+RexsortRecursive(A, i, right, mask-1)
 \\Code}
 
 \\Code{
@@ -94,12 +81,12 @@ while i < j \\B 303
         if we use "break" or similar to exit from this loop.
 \\Expl}
 \\In{
-    Repeatedly increment i until i >= j or A[i] has 1 as the mask bit \\Ref PartitionLeft
+    Repeatedly increment i until i >= j or A[i] has 1 as the mask bit \\B 304
     \\Expl{ Scan right looking for a "large" element that is out of
         place. Bitwise "and" between A[i] and mask can be used to
         extract the desired bit.
     \\Expl}
-    Repeatedly decrement j until j <= i or A[j] has 0 as the mask bit \\Ref PartitionRight
+    Repeatedly decrement j until j <= i or A[j] has 0 as the mask bit \\B 305
     \\Expl{ Scan left looking for a "small" element that is out of
         place. Bitwise "and" between A[i] and mask can be used to
         extract the desired bit.
@@ -114,25 +101,6 @@ while i < j \\B 303
         \\Expl}
     \\In}
 \\In}
-\\Code}
-
-\\Code{
-PartitionLeft
-while (i <= right && ((arr[i] >> mask & 1)) === 0) { \\B 304
-  \\In{
-    i++
-  \\In}
-}
-\\Code}
-
-
-\\Code{
-PartitionRight
-while (j >= left && ((arr[j] >> mask & 1)) === 1) { \\B 305
-  \\In{
-    j--
-  \\In}
-}
 \\Code}
 
 \\Code{
